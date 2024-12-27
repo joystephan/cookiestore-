424787270d9a3524da0d89685d0efd4ec52246a2
joystephan <jestephan931@gmail.com>
2024-12-27 16:42:11 +0200

    main functionalities completed

--- app/src/main/java/com/example/coolkies/MainActivity.java
+++ app/src/main/java/com/example/coolkies/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.coolkies;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
@@ -9,12 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,57 +28,51 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.navigation_view);
 
-        if (drawerLayout == null || navigationView == null) {
-            throw new RuntimeException("DrawerLayout or NavigationView not found in layout");
-        }
-
-        // Set up Drawer toggle
-        toggle = new ActionBarDrawerToggle(
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
         // Handle navigation menu item clicks
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.nav_home) {
-                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
-                } else if (id == R.id.nav_menu) {
-                    startActivity(new Intent(MainActivity.this, MenuActivity.class));
-                } else if (id == R.id.nav_contact) {
-                    startActivity(new Intent(MainActivity.this, ContactActivity.class));
-                }
-                // Close the drawer after a menu item is selected
-                drawerLayout.closeDrawer(GravityCompat.START);
-                return true;
-            }
+        navigationView.setNavigationItemSelectedListener(item -> {
+            selectFragment(item);
+            return true;
         });
 
-        // Set default fragment/activity as the Home page (welcome page)
+        // Set default fragment
         if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
             navigationView.setCheckedItem(R.id.nav_home);
-            startActivity(new Intent(MainActivity.this, HomeActivity.class));
         }
+    }
+
+    private void selectFragment(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+
+        if (item.getItemId() == R.id.nav_home) {
+            selectedFragment = new HomeFragment();
+        } else if (item.getItemId() == R.id.nav_menu) {
+            selectedFragment = new MenuFragment();
+        } else if (item.getItemId() == R.id.nav_contact) {
+            selectedFragment = new ContactFragment();
+        }
+
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
     }
 
     @Override
     public void onBackPressed() {
-        // Close the drawer if it's open
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        // Handle toggle click events
-        if (toggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
